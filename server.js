beab7e4da9795fa7b47e531e57b125cfe097f156
Usman <usmanbinkhalidpk@gmail.com>
2026-04-19 09:48:09 +0500

    Running Errors Solved

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ app.use('/api', profileRoutes);
 // Error handling middleware (must be last)
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
